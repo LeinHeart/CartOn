4a4474be9078bd4ee35bc304037b0c79768d7743
Kristopher Chayadi <kristopher.chayadi1688@gmail.com>
2018-09-25 14:35:57 +0700

    Initial Commit

---
+++ CartOn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CartOn
+//
+//  Created by Kristopher Chayadi on 25/09/18.
+//  Copyright © 2018 Kristopher Chayadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
