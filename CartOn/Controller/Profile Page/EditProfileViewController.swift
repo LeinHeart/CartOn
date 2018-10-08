c7dc7d19dba36efb7b3cb06f3a2ce36dfaba878f
Ricky Adi Kuncoro <riq.adi@gmail.com>
2018-10-08 20:55:19 +0700

    merge everything (Ricky, Krist)

--- CartOn/Controller/Profile Page/EditProfileViewController.swift
+++ CartOn/Controller/Profile Page/EditProfileViewController.swift
@@ -15,18 +15,6 @@ class EditProfileViewController: UIViewController {
         
         self.navigationItem.title = "Edit Profile"
         self.view.backgroundColor = .blue
-
-        // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
