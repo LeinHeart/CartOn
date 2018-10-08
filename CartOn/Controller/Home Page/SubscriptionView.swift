e866e23fd81c313f86f520f4a79ae47ff498381d
Kristopher Chayadi <kristopher.chayadi1688@gmail.com>
2018-10-08 15:45:45 +0700

    Filenya ketinggalan

--- CartOn/Controller/Home Page/SubscriptionView.swift
+++ CartOn/Controller/Home Page/SubscriptionView.swift
@@ -1,5 +1,5 @@
 //
-//  SubscriptionView.swift
+//  PopularView.swift
 //  CartOn
 //
 //  Created by Kristopher Chayadi on 08/10/18.
@@ -8,8 +8,58 @@
 
 import UIKit
 
-class SubscriptionView: UIView {
-
+class SubscriptionView: UIView,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    
+    
+    let subcriptionCellId = "subscriptionCellId"
+    let subscriptionImageArray=["sub1","sub2","sub3","sub4","sub5"]
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 16
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .clear
+        return cv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        print("Ter init")
+        addSubview(collectionView)
+        collectionView.setAnchor(top: self.topAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(SubscriptionViewCell.self, forCellWithReuseIdentifier: subcriptionCellId)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return subscriptionImageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: subcriptionCellId, for: indexPath) as! SubscriptionViewCell
+        cell.subscriptionNames = subscriptionImageArray[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (self.frame.width / 2) - 20 , height: (self.frame.width / 2) * 1.25)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
--- CartOn/Controller/Home Page/SubscriptionViewCell.swift
+++ CartOn/Controller/Home Page/SubscriptionViewCell.swift
@@ -6,4 +6,77 @@
 //  Copyright © 2018 Kristopher Chayadi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SubscriptionViewCell: UICollectionViewCell{
+    var subscriptionNames: String? {
+        didSet{
+            if let imageName = subscriptionNames {
+                imageView.image = UIImage(named: imageName)
+            }
+        }
+    }
+    
+    let imageView:UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.layer.cornerRadius = 15
+        return iv
+    }()
+    
+    let titleLabel : UILabel = {
+        let tl = UILabel()
+        tl.font = UIFont(name: "Avenir-medium", size: 11)
+        tl.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        return tl
+    }()
+    let uploaderLabel : UILabel = {
+        let tl = UILabel()
+        tl.font = UIFont(name: "Avenir-medium", size: 11)
+        tl.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        return tl
+    }()
+    let likeCount = UILabel.init()
+    let likeIcon = UIImageView.init()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    func setup(){
+        backgroundColor = .white
+        //backgroundColor = .clear
+        setCellShadow()
+        
+        uploaderLabel.text = "Nama Uploader"
+        uploaderLabel.frame = CGRect(x: 0, y: 0, width: 150, height: 20)
+        
+        titleLabel.text = "Title Gambar"
+        
+        
+        likeCount.text = "100"
+        likeCount.font = UIFont(name: "Avenir-Medium", size: 11)
+        likeCount.frame = CGRect(x: 0, y: 0, width: 100, height: 10)
+        
+        likeIcon.image = UIImage(named: "like")
+        likeIcon.frame = CGRect(x: 0, y: 0, width: 18, height: 18)
+        
+        addSubview(uploaderLabel)
+        addSubview(titleLabel)
+        addSubview(imageView)
+        addSubview(likeIcon)
+        addSubview(likeCount)
+        
+        uploaderLabel.setAnchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 10, paddingBottom: 0, paddingRight: 0)
+        imageView.setAnchor(top: uploaderLabel.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0,width: self.frame.size.width,height: self.frame.size.height / 2 * 1.35)
+        titleLabel.setAnchor(top: imageView.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 10, paddingLeft: 10, paddingBottom: 0, paddingRight: 0)
+        likeIcon.setAnchor(top: nil, left: self.leftAnchor, bottom: self.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 10, paddingBottom: -10, paddingRight: 0,width: 18,height: 18)
+        likeCount.setAnchor(top: nil, left: likeIcon.rightAnchor, bottom: self.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 10, paddingBottom: -10, paddingRight: 0 )
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
